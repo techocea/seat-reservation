d13bd73bffae5f12fba2fd4c1c1274eece52255e
techocea <dunsfordbright@gmail.com>
2025-02-17 03:14:06 +0530

    vite config files

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "https://sl-highway.netlify.app",
-    port: 3000,
-  },
 });
